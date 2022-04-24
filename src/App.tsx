7f1a92e7f830f75b0c90c46241e7b7240b2d35f3
Zdeněk Mazurák <z.mazurak@seznam.cz>
2022-04-24 19:04:54 +0200

    first steps

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,16 @@
 import React from 'react'
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import {autowired, component } from 'ironbean'
-import { useBean } from 'ironbean-react'
+import style from "./style.module.less";
 
-@component
-class B {
-
-}
-
-@component
-class A {
-   @autowired b!: B;
-}
 
 function App() {
-  const [count, setCount] = useState(0)
-  const a = useBean(A);
-
-  console.log(a.b)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>ČAUKY</p>
-        <p>
-          <button type="button"  className={""} onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className={style.layout}>
+      <div className={style.left}>
+        <div className={style.user}>
+            <img src={"https://picsum.photos/id/237/200/300"} className={style.photo}></img>
+            <h2 className={style.name}>Zdeněk Mazurák</h2>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/global.d.ts
@@ -0,0 +1,6 @@
+declare module "*.module.less" {
+    const content: {
+        [key: string]: string
+    };
+    export default content;
+}
